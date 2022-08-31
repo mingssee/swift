7f57f7df966aa20d77d9bca23177c97515d35b87
mingssee <103242836+mingssee@users.noreply.github.com>
2022-08-31 21:16:12 +0900

    Add files via upload

---
+++ 0423SocialMedia-main/moviemovie/moviemovie/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  moviemovie
+//
+//  Created by 정민주 on 2022/05/09.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func btnPlayInternMovie(_ sender: UIButton) {
+        //내부 파일 mp4
+        let filePath:String? = Bundle.main.path(forResource: "FastTyping", ofType: "mp4")
+        let url = NSURL(fileURLWithPath: filePath!)
+        
+        playVideo(url: url)
+        let playerController = AVPlayerViewController()
+        
+        let player = AVPlayer(url: url as URL)
+        playerController.player = player
+        
+        self.present(playerController, animated: true) {
+            player.play()
+        }
+    }
+    
+    private func playVideo(url: NSURL) {
+        let playerController = AVPlayerViewController()
+        
+        let player = AVPlayer(url: url as URL)
+        playerController.player = player
+        
+        self.present(playerController, animated: true) {
+            player.play()
+        }
+    }
+}
+
